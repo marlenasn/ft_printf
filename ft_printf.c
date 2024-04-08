37c1cd04a33ec593606e45686e93a99a1a91d0da
Marlena Wiecek <mwiecek@c2r7s4.42warsaw.pl>
2024-04-08 11:38:35 +0200

    second

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mwiecek <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 15:31:44 by mwiecek           #+#    #+#             */
-/*   Updated: 2024/04/07 21:02:39 by mwiecek          ###   ########.fr       */
+/*   Updated: 2024/04/08 10:58:28 by mwiecek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,60 +15,59 @@
 #include <unistd.h>
 #include <limits.h>
 
-static int    ft_putchar(char c)
+static int	ft_putchar(char c)
 {
-        write(1, &c, 1);
-        return (1);
+	write(1, &c, 1);
+	return (1);
 }
 
-static int    ft_putnbr(int n)
+static int	ft_putnbr(int n)
 {
-        char    a;
-        int	len;
-	
+	char	a;
+	int		len;
+
 	len = 0;
-        if (n == -2147483648)
-        {
-                write (1, "-2", 2);
-                n = 147483648;
-		len += 2; // - and 2
-        }
-        if (n < 0)
-        {
-                write (1, "-", 1);
-                n = -1 * n;
+	if (n == -2147483648)
+	{
+		write (1, "-2", 2);
+		n = 147483648;
+		len += 2;
+	}
+	if (n < 0)
+	{
+		write (1, "-", 1);
+		n = -1 * n;
 		len++;
-        }
-
-        if (n >= 10)
-        {
-                len = len + ft_putnbr(n / 10);
-                ft_putnbr(n % 10);
+	}
+	if (n >= 10)
+	{
+		len = len + ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
 		len++;
-        }
-        else
-        {
-                a = n + '0';
-                write (1, &a, 1);
+	}
+	else
+	{
+		a = n + '0';
+		write (1, &a, 1);
 		len++;
-        }
-        return (len);
+	}
+	return (len);
 }
 
-static int    ft_putstr(char *s)
+static int	ft_putstr(char *s)
 {
 	int	len;
-	
+
 	len = 0;
-        if (s != NULL)
-        {
-                while (*s)
-                {
-                        write(1, s, 1);
-                        len++;
-                        s++;
-                }
-        }
+	if (s != NULL)
+	{
+		while (*s)
+		{
+			write(1, s, 1);
+			len++;
+			s++;
+		}
+	}
 	else
 	{
 		write(1, "(null)", 6);
@@ -79,27 +78,27 @@ static int    ft_putstr(char *s)
 
 static int	ft_print_ptr_chars(unsigned long n)
 {
-        char    	a;
+        char	a;
 	int	len;
-	len = 1;
 
-        if (n >= 16)
-        {
-                len = len + ft_print_ptr_chars(n / 16);
-                ft_print_ptr_chars(n % 16);
-        }
-        else
-        {
-                if (n < 10)
-                        a = n + '0';
-                else
-                        a = (n - 10) + 'a';
-                write (1, &a, 1);
-        }
-        return (len);
+	len = 1;
+	if (n >= 16)
+	{
+		len = len + ft_print_ptr_chars(n / 16);
+		ft_print_ptr_chars(n % 16);
+	}
+	else
+	{
+		if (n < 10)
+			a = n + '0';
+		else
+			a = (n - 10) + 'a';
+		write (1, &a, 1);
+	}
+	return (len);
 }
 
-static int    ft_print_ptr(unsigned long ptr)
+static int	ft_print_ptr(unsigned long ptr)
 {
 	unsigned long	n;
 
@@ -108,80 +107,78 @@ static int    ft_print_ptr(unsigned long ptr)
 		write(1, "(nil)", 5);
 		return (5);
 	}
-
 	n = (unsigned long)ptr;
 	write(1, "0x", 2);
-	// 2 + to include "Ox" characters for the first execution
 	return (2 + ft_print_ptr_chars(n)); 
 }
 
-static int    ft_put_X(unsigned int n)
+static int	ft_put_X(unsigned int n)
 {
-        char    a;
-        int	len;
+	char	a;
+	int		len;
 
 	len = 1;
-        if (n >= 16)
-        {
-                len = len + ft_put_X(n / 16);
-                ft_put_X(n % 16);
-        }
-        else
-        {
-                if (n < 10)
-                        a = n + '0';
-                else
-                        a = n + 55;
-                write (1, &a, 1);
-        }
-        return (len);
+	if (n >= 16)
+	{
+		len = len + ft_put_X(n / 16);
+		ft_put_X(n % 16);
+	}
+	else
+	{
+		if (n < 10)
+			a = n + '0';
+		else
+			a = n + 55;
+		write (1, &a, 1);
+	}
+	return (len);
 }
 
-static int    ft_put_x(unsigned int n)
+static int	ft_put_x(unsigned int n)
 {
-        char    a;
-        int	len;
+        char	a;
+	int	len;
 
 	len = 1;
-        if (n >= 16)
-        {
-                len = len + ft_put_x(n / 16);
-                ft_put_x(n % 16);
-        }
-        else
-        {
-                if (n < 10)
-                        a = n + '0';
-                else
-                        a = (n - 10) + 'a';
-                write (1, &a, 1);
-        }
-        return (len);
+	if (n >= 16)
+	{
+		len = len + ft_put_x(n / 16);
+		ft_put_x(n % 16);
+	}
+	else
+	{
+		if (n < 10)
+			a = n + '0';
+		else
+			a = (n - 10) + 'a';
+		write (1, &a, 1);
+	}
+	return (len);
 }
 
-static int    ft_put_un_nbr(unsigned int n)
+static int	ft_put_un_nbr(unsigned int n)
 {
-        char    a;
-        int	len;
-        
-        len = 1;
-        if (n >= 10)
-        {
-                len = len + ft_put_un_nbr(n / 10);
-                ft_put_un_nbr(n % 10);
-        }
-        else
-        {
-                a = n + '0';
-                write (1, &a, 1);
-        }
-        return (len);
+	char	a;
+	int		len;
+
+	len = 1;
+	if (n >= 10)
+	{
+		len = len + ft_put_un_nbr(n / 10);
+		ft_put_un_nbr(n % 10);
+	}
+	else
+	{
+		a = n + '0';
+		write (1, &a, 1);
+	}
+	return (len);
 }
 
 static int	what_char(char c, va_list arg)
 {
 	int	len;
-	
+
 	len = 0;
 	if (c == 'c')
 		len = len + ft_putchar(va_arg(arg, int));
@@ -208,7 +205,7 @@ static int	what_char(char c, va_list arg)
 int	ft_printf(const char *s, ...)
 {
 	va_list	arg;
-	int	len;
+	int		len;
 
 	va_start(arg, s);
 	len = 0;
@@ -230,7 +227,6 @@ int	ft_printf(const char *s, ...)
 	va_end(arg);
 	return (len);
 }
-
 /*
 int	main(void)
 {
